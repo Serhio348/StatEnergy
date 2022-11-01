02eecd6e60433a9f108663149a01750c4466411e
Serhio348 <serhiosidorovich@gmail.com>
2022-11-01 14:14:47 +0300

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.scss'
+
+const App: React.FC = () => {
+
+  return (
+    <div className="app-container">
+      <div className="app-content">
+
+      </div>
+
+    </div>
+
+  );
+};
+
+export default App;
